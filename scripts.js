8b14fe2db3c15bbe938b181fac6fbfddb13936db
MirandaMcCoy <mm0778076@otc.com>
2017-09-14 23:55:11 -0500

    Renamed AboutUs to index Added (temporary?) logo Added function that changes the logo on click Removed hovering text color changer Started messing with possible color combos

--- scripts.js
+++ scripts.js
@@ -1,15 +1,8 @@
-document.getElementById("seeSelectionButton").addEventListener("click", function(){ window.location.href = "Gallery.htm"; }, false);
-document.getElementById("bookCaterButton").addEventListener("click", function(){ window.location.href = "ContactUs.htm"; }, false);
+var logo = document.getElementById("logo");
 
-document.getElementById("welcome").addEventListener("mouseenter", changeColorTo, false);
-document.getElementById("welcome").addEventListener("mouseleave", changeColorBack, false);
+logo.addEventListener("click", biteLogo, false);
 
 // Admittedly placeholder function
-function changeColorTo(){
-  document.getElementById("welcome").className = 'purpleText'; // Probably a better way to do this, but it's almost 3am and I'm ready for bed
-}
-
-// Admittedly placeholder function
-function changeColorBack(){
-  document.getElementById("welcome").className = 'blackText';
+function biteLogo(){
+  logo.src = "images/logo/logobite.png"
 }
